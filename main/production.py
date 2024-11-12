592eb069087ea1bffa2144337df07fad8a2dbb01
SashaAskub2003 <sashkabach@gmail.com>
2024-11-12 15:01:24 +0200

    add secure

--- main/production.py
+++ main/production.py
@@ -3,3 +3,7 @@ from .settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['teststep.herokuapp.com']
+
+SESSION_COOKIE_SECURE = True  # Для работы через HTTPS
+CSRF_COOKIE_SECURE = True     # Для защиты от CSRF-атак
+SECURE_SSL_REDIRECT = True
