c0f38e71e6b5a8c37a6ce0403d7b2f886edef271
Saymios <daniojenezis@gmail.com>
2025-06-06 11:41:56 +0300

    fix not question text from matching type question in take test and add duration filed in edit test page

---
+++ tests/static/js/check_duration_field.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const durationField = document.getElementById('durationField');
+    const testForm = document.getElementById('form-edit-test');
+    const saveBtn = document.querySelector('.save-btn');
+
+    durationField.addEventListener('change', () => {
+        const duration = parseInt(durationField.value, 10)
+        
+        if (duration && duration < 1440) {
+            saveBtn.disabled = false;
+        } else {
+            saveBtn.disabled = true;
+        }
+    })
+
+    // testForm.addEventListener('submit', (e) => {
+    //     e.preventDefault()
+
+    //     const duration = parseInt(durationField.value, 10)
+
+    //     if (duration) {
+    //         testForm.submit();
+    //     }
+    // })
+})
