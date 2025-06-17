6effa9d4fde65ebc105537011eae4f43744f8140
Saymios <daniojenezis@gmail.com>
2025-06-17 10:16:34 +0300

    add two signals for test in project

--- tests/apps.py
+++ tests/apps.py
@@ -5,3 +5,6 @@ class TestsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tests'
     verbose_name = 'Тести'
+
+    def ready(self):
+        import tests.signals
